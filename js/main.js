e4adc75642b740f60434db07a656818866b441b5
colleenmcginnis <colleen.j.mcginnis@gmail.com>
2015-11-04 22:50:48 -0600

    adding images

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('#researchTitle').on('click', function () {
+    $('#researchTitle').css('opacity', 1);
+    $('#researchText').slideToggle();
+});
+
+$('#auditTitle').on('click', function () {
+    $('#auditTitle').css('opacity', 1);
+    $('#auditText').slideToggle();
+});
+
+$('#designTitle').on('click', function () {
+    $('#designTitle').css('opacity', 1);
+    $('#designText').slideToggle();
+});
+
+$('#pilotTitle').on('click', function () {
+    $('#pilotTitle').css('opacity', 1);
+    $('#pilotText').slideToggle();
+});
